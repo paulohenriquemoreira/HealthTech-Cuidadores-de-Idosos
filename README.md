801f4b862f07ade4240d22cf03ec30813107a35f
paulohenriquemoreira <paulo.henrick.moreira@hotmail.com>
2026-04-12 23:36:17 -0300

    Finalizado Verbos Get, Post, Put,Delete

--- README.md
+++ README.md
@@ -2,3 +2,126 @@
 
 ### A proposta é trabalhar a partir de um cenário inspirado no mundo profissional, onde o problema não está totalmente definido, e cabe a você interpretar, analisar e propor uma solução.
 
+## 🚀 Tecnologias Utilizadas
+
+<p align="left">
+    <img
+        height="65"
+        src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/express.png"
+        title="Express"
+    />
+    <img
+        height="65"
+        src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/node_js.png"
+        title="Nodejs"
+    />
+    <img
+        height="65"
+        src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/postman.png"
+        title="Postman"
+    />
+    <img
+        height="65"
+        src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/sqlite.png"
+        title="SQLite/SQLite3"
+    />
+</p>
+
+## ▶️ Como Rodar o Projeto
+
+Siga os passos abaixo para rodar a aplicação localmente:
+
+1. **Clone esse repositório:**
+
+   ```
+   git clone https://github.com/paulohenriquemoreira/HealthTech-Cuidadores-de-Idosos
+
+   cd HealthTech-Cuidadores-de-Idosos
+   ```
+
+2. **Instale as dependências:**
+
+   ```
+   npm install
+   npm express
+   npm sqlite3
+   npm sqlite
+   ```
+
+3. **Inicie o servidor de desenvolvimento:**
+
+   ```
+   npm run dev
+   **Acesse a aplicação no navegador:** http://localhost:3000/
+   ```
+
+## 🗄️ Banco de Dados
+
+O banco de dados é criado automaticamente ao iniciar o projeto
+
+```
+paciente.db
+```
+
+## 📋 Estrutura da Tabela (Paciente)
+
+| Campo            | Descrição                 |
+| ---------------- | ------------------------- |
+| id               | Identificador único       |
+| nome             | Nome do(a) Paciente       |
+| idade            | Idade do(a) Paciente      |
+| descricao        | Informações do(a) Paciente |
+| estado_saude     | Estado atual saúde do Paciente |
+| autonomia        | Autonomia do Paciente     |
+| medicamentos     | Medicamentos em uso       |
+| alergias         | Tipos de alergia          |
+| data_atendimento | Data do atendimento       |
+| endereco_residencia | Endereço de moradia    |
+| contato_emergencia  | Contato para emergência|
+
+
+## 🔗 Endpoints
+
+### 📍 Rota inicial
+
+```
+GET /
+```
+
+Retorna uma página HTML com informações da API
+
+### 📄 Listar Idosos
+
+```
+GET /pacientes
+```
+
+### 🔍 Buscar idosos (id)
+
+```
+GET /paciente/:id
+```
+
+Ex: <code>/paciente/1</code>
+
+### ➕ Criar cadastro de paciente
+
+```
+POST /paciente
+```
+
+### ✏️ Atualizar cadastro de paciente
+
+```
+PUT /paciente/:id
+```
+
+### ❌ Deletar cadastro de paciente
+
+```
+DELETE /paciente/:id
+```
+
+## 🎯 Objetivo do Projeto
+
+Este projeto foi desenvolvido com foco em aprendizado de Back End utilizando Node.js e boas práticas no desenvolvimento de APIs.
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 //Importar o framework express
-const express = require('express')
+const express = require('express');
 
 //Criando a aplicação (servidor)
-const app = express()
+const app = express();
 
 //Configura o express para entender os dados enviados no formato JSON.
 app.use(express.json());
 
-//Rota principal
+//Rota principal (/)
 app.get("/", (req, res) => {
 
     res.send(`
@@ -18,16 +18,87 @@ app.get("/", (req, res) => {
         </body>
     `)
 
-})
+});
 
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
     
-    console.log(`Servidor rodando na porta http://localhost:${PORT}`);
+    console.log(`Servidor rodando na porta http:localhost:${PORT}`);
     
 });
 
+//Rota de Pacientes
+
+
+const listaPacientes = [
+    {
+        id:1,
+        nome:"João Roberto",
+        idade:"55 anos",
+        descricao:"Foi medicado corretamente,paciente teve um dia calmo com leitura.",
+        estado_saude:"Houve uma melhora referente aos picos de pressão arterial",
+        autonomia:"Se alimentou e fez atividades sem auxílio.",
+        medicamentos:"Sinvastatina (20:00, 20mg, para Colesterol Alto) e Losartana (08:00, 50mg, para Hipertensão)",
+        alergias:"Penicilina",
+        data_atendimento:"06/04/2026",
+        endereco_residencia:"Rua das Flores, 123 - Centro",
+        contato_emergencia:"(11) 98765-4321",
+    },
+    {
+        id:2,
+        nome:"Ana Castela",
+        idade:"67 anos",
+        descricao:"Foi medicada no horário,paciente teve um dia sem intercorrências.",
+        estado_saude:"Demonstrou piora no quadro de Alzheimer.",
+        autonomia:"Se alimentou e fez atividades sem auxílio.",
+        medicamentos:"Donepezila (22:00, 10mg, para Alzheimer)",
+        alergias:"Ácido Acetilsalicílico (Aspirina)",
+        data_atendimento:"07/04/2026",
+        endereco_residencia:"Av. Brasil, 1500 - Jardim América",
+        contato_emergencia:"(11) 93322-1144",
+    }
+]
+
+
+app.get("/pacientes", (req, res) => {
+  
+    res.json(listaPacientes);
+
+});
+
+//Parâmetros de Rota de Pacientes
+app.get("/pacientes/:id", (req, res) => {
+
+    const idPegoURL = Number(req.params.id);
+    const pacienteEncontrado = listaPacientes.find((listaPacientes) => listaPacientes.id === idPegoURL);
+
+    res.json(pacienteEncontrado);
+
+});
+
+
+//Rota de Novos Pacientes (Verbo POST)
+
+app.post("/pacientes",(req, res) => {
+
+  const novoPaciente = {
+    id: listaPacientes.length + 1,
+    nome: req.body.nome,
+    idade:req.body.idade,
+    descricao:req.body.descricao,
+    estado_saude:req.body.estado_saude,
+    autonomia:req.body.autonomia,
+    medicamentos:req.body.medicamentos,
+    alergias:req.body.alergias,
+    data_atendimento:req.body.data_atendimento,
+    endereco_residencia:req.body.endereco_residencia,
+    contato_emergencia:req.body.contato_emergencia,
+  };
+
+  listaPacientes.push(novoPaciente);
+  res.send(`Paciente ${novoPaciente.nome} cadastrado com sucesso!`);
+});
 
 
