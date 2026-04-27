75cdf2c6c745632c1b319f027b599f06cc56b373
Paulo Henrique Moreira <100167872+paulohenriquemoreira@users.noreply.github.com>
2026-04-27 16:46:49 -0300

    Add CORS support and update PORT configuration

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 //Importar o framework express
 const express = require('express');
+const cors = require("cors");
 
 //Criando a aplicação (servidor)
 const app = express();
+app.use(cors());
 
 //Configura o express para entender os dados enviados no formato JSON.
 app.use(express.json());
@@ -21,12 +23,9 @@ app.get("/", (req, res) => {
 });
 
 
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    
-    console.log(`Servidor rodando na porta http:localhost:${PORT}`);
-    
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
 //Rota de Pacientes
